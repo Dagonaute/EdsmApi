5ca5fdbea16aae0a5bc733c50b041d28a86c26f8
tbayart <tbayart@nordnet.fr>
2022-04-15 21:56:05 +0200

    Added EdsmQuerySystem : Get information about a system - https://www.edsm.net/en/api-v1

---
+++ EdsmApi/EdsmQuerySystem.cs
@@ -0,0 +1,22 @@
+﻿using EdsmApi.Interface;
+using EdsmApi.Models;
+using System.Runtime.Serialization;
+
+namespace EdsmApi
+{
+    public class EdsmQuerySystem : EdsmQueryApiV1, IEdsmQuery<EdsmSystem>
+    {
+        private static string[] _querySegment = { "system" };
+
+        public EdsmQuerySystem()
+            : base(_querySegment)
+        {
+        }
+
+        [DataMember(Name = EdsmQueryParameterName.showInformation)]
+        public bool ShowInformation { get; set; } = true;
+
+        [DataMember(Name = EdsmQueryParameterName.showPermit)]
+        public bool ShowPermit { get; set; } = true;
+    }
+}
