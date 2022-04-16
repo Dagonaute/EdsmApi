3e53a6981ec8fdd36f277beb88f8033c24441f8e
tbayart <tbayart@nordnet.fr>
2022-04-16 10:16:06 +0200

    Added EdsmQueryBodies : Get information about celestial bodies in a system - https://www.edsm.net/en/api-system-v1

---
+++ EdsmApi/EdsmQueryBodies.cs
@@ -0,0 +1,18 @@
+﻿using EdsmApi.Interface;
+using EdsmApi.Models;
+
+namespace EdsmApi
+{
+    /// <summary>
+    /// Get information about celestial bodies in a system - https://www.edsm.net/en/api-system-v1
+    /// </summary>
+    public class EdsmQueryBodies : EdsmQueryApiSystemV1, IEdsmQuery<EdsmSystemBodies>
+    {
+        private static string[] _querySegment = { "bodies" };
+
+        public EdsmQueryBodies()
+            : base(_querySegment)
+        {
+        }
+    }
+}
