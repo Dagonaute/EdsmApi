963f62c2d27084a0d2065ca6cabc5634a8000a1f
tbayart <tbayart@nordnet.fr>
2022-04-18 20:20:41 +0200

    Added EdsmQuerySphereSystems : Get systems in a sphere radius - https://www.edsm.net/en/api-v1

---
+++ EdsmApi/EdsmQuerySphereSystems.cs
@@ -0,0 +1,27 @@
+﻿using EdsmApi.Interface;
+using EdsmApi.Models;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace EdsmApi
+{
+    /// <summary>
+    /// Get systems in a sphere radius - https://www.edsm.net/en/api-v1
+    /// </summary>
+    public class EdsmQuerySphereSystems : EdsmQueryApiV1, IEdsmQuery<List<EdsmSystem>>
+    {
+        private static string[] _querySegment = { "sphere-systems" };
+
+        public EdsmQuerySphereSystems()
+            : base(_querySegment)
+        {
+        }
+
+        /// <summary>
+        /// The desired radius in ly.
+        /// Maximum value is 100.
+        /// </summary>
+        [DataMember(Name = EdsmQueryParameterName.radius)]
+        public int Radius { get; set; }
+    }
+}
